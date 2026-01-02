8ef8cd7323a0220b0142622c67486098783b4597
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2026-01-02 19:08:09 +0530

    resnet update

--- src/models/resnet_pytorch.py
+++ src/models/resnet_pytorch.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import gc
 import torch.nn as nn
 from torchvision import datasets
 from torchvision import transforms
@@ -32,7 +33,7 @@ def data_loader(
         if test:
             dataset = datasets.CIFAR10(
               root=data_dir, train=False,
-              download=True, transform=transform,
+              download=False, transform=transform,
             )
 
             data_loader = torch.utils.data.DataLoader(
@@ -44,12 +45,12 @@ def data_loader(
         # load the dataset
         train_dataset = datasets.CIFAR10(
             root=data_dir, train=True,
-            download=True, transform=transform,
+            download=False, transform=transform,
         )
 
         valid_dataset = datasets.CIFAR10(
             root=data_dir, train=True,
-            download=True, transform=transform,
+            download=False, transform=transform,
         )
 
         num_train = len(train_dataset)
@@ -75,3 +76,127 @@ def data_loader(
 train_loader, valid_loader = data_loader(data_dir='./data', batch_size=64)
 
 test_loader = data_loader(data_dir='./data', batch_size=64, test=True)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+
+        if self.downsample:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+        return out
+    
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+        self.max_pool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avg_pool = nn.AvgPool2d(7, (1, 1))
+        self.fc = nn.Linear(512, num_classes)
+    
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+        
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avg_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+    
+num_classes = 10
+num_epochs = 20
+batch_size = 16
+learning_rate = 0.01
+
+model = ResNet(ResidualBlock, [3, 4, 6, 3]).to(device)
+
+#Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.001, momentum = 0.9)  
+
+#Train the model
+total_step = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        if i>=10:
+            break
+        print(i)
+        #Move tensors to the configured device
+        images = images.to(device)
+        labels = labels.to(device)
+
+        #Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        #Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        del images, labels, outputs
+        torch.cuda.empty_cache()
+        gc.collect()
+
+    print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+
+#Validation
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in valid_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        del images, labels, outputs
+
+    print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total))
